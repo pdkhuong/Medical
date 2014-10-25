7b86451773250e713551bd07f4dfd59ddfb5ba27
KhuongPham <pham_dinhkhuong_tt@yahoo.com>
2014-10-26 01:14:51 +0700

    delete

--- addons/shared_addons/themes/medical/js/medical.js
+++
@@ -1,5 +0,0 @@
-var Medical = {
-    init: function(){
-        console.debug("init");
-    }
-}
--- addons/shared_addons/themes/medical/js/scripts.js
+++
@@ -1,8 +0,0 @@
-/*
- * Title:   Travelo | Responsive HTML5 Travel Template - Custom Javascript file
- * Author:  http://themeforest.net/user/soaptheme
- */
-
-tjq(document).ready(function() {
-    // UI Form Element
-});
